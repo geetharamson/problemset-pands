a0c8da677e8ab21ed16cb636c6517f09fc865179
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-02-14 11:49:04 +0000

    Update p2.py

--- p2.py
+++ p2.py
@@ -3,6 +3,6 @@
 import datetime
 
 if datetime.datetime.today().weekday() == [1,3]:
-  print("Yes -today begins with a T.")
+  print("Yes-today begins with a T.")
 else:
   print("No-today doesnot begin with a T.")
