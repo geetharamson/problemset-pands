7fb29a28571e2a83048ccd8ec09aa2711a008493
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-02-14 11:46:33 +0000

    problem 2

---
+++ p2.py
@@ -0,0 +1,8 @@
+# Is today a day that begins with the letter T 
+
+import datetime
+
+if datetime.datetime.today().weekday() == [1,3]:
+  print("Yes -today begins with a T.")
+else:
+  print("No-today doesnot begin with a T.")
