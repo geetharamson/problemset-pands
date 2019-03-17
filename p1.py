f65b3d50087ddb83afc747a5dcd07bc353c6ddb2
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-03-17 01:26:24 +0000

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,11 +1,15 @@
+# Geetha Karthikesan, 2019
 #sumupto a given number 
-
+# inputing a positive integer  
 i= int(input("please enter a positive integer:"))
+# entering value of sum as 0
 
 sum = 0
-
-while i > 0:
+#checking whether i is greater than 0
+while i >0:
+  #value of  sum becomes sum +i
   sum = sum + i
+  # new value of i becomes i-1
   i = i-1
-
+# print sum 
 print (sum)
