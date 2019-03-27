5e476de06d894a3c630755a77bf19640ce3e386c
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-03-27 21:53:40 +0000

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,15 @@
+# Geetha Karthikesan ,2019
+# squareroot  of positive floating number 
+ # Input value of x as the floating point value of no thats entered
+x= float (input("please enter a positive floating point number:"))
+
+#  importing math module
+import math  
+#Value of y is the square root of x
+y = math.sqrt(x)
+
+# To print the output of the square root of floating point no
+# Since print command has strings in it, str() operation is used for x and y values.
+ 
+print (" The square root of : " + str(x) +  " is approx: " + str (y)) 
+
