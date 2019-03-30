7d0a05f8db3a56a47ac0d51e7769526783f767b6
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-03-30 12:35:56 +0000

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,28 @@
+# Geetha Karthikesan , 2019
+# p9.py mody-txt
+# Import sys
+import sys 
+
+# Input value of fileName as sys.argv[1]
+fileName =sys.argv[1]
+
+#print fileName 
+print (fileName)
+
+# Opening  fileName and reading 
+fileObject = open (fileName, 'r')
+
+#Initialising index as 1
+index = 1
+
+# Using for loop to print alternate lines 
+for line in fileObject :
+    index = index + 1
+    
+    if index % 2 == 0:
+     # print line and omit the blank space      
+      print(line.rstrip())
+
+
+# Closes the fileObject
+fileObject.close()
