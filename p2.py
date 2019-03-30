e53241c68e64ea392963b1215541613e8ee71913
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-03-30 12:53:47 +0000

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,8 +1,11 @@
+# Geetha Karthikesan, 2019 
 # Is today a day that begins with the letter T 
-
+# Import datetime
 import datetime
-
-if datetime.datetime.today().weekday() == [1,3]:
+# if today is a weekday like Tuesday or Thursday
+if datetime.datetime.today().weekday() == [2,4]:
+  # then print yes today begins with a T
   print("Yes-today begins with a T.")
 else:
+  #else print no-today doesnot begin with a T
   print("No-today doesnot begin with a T.")
