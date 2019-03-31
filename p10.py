6495a9977c50e89e9136317c9897c70876c29b5b
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-03-31 10:27:43 +0100

    Update p10.py

--- p10.py
+++ p10.py
@@ -1,18 +1,12 @@
 # Geetha Karthikesan ,2019
 #program to display a plot function  x ,square of x and 2 exponent x in range (0,4)
 
-for x in range (0,4):
-    # to find square of no in the range 
- squares =(x ** 2 for x in range (0,4)) 
-
- # to find 2 power of a no 
- power = pow (2,x)
-print ( x, squares , power ) 
-
-def (number)
 
 import matplotlib.pyplot as plt
-plt.plot([0,1,2,3,4][x,x**2,2**x])
+input_values = [0,1,2,3,4]
+squares = [0,1,4,9,16]
+exp =[1,2,4,8,16]
+plt.plot(input_values,squares,exp ,linewidth = 5)
 plt.show() 
 
 
